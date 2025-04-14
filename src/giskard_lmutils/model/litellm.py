a2fced1e67f249ec4c81c3a0583c4f6f8aa4136b
Kevin Messiaen <kevin.messiaen@icloud.com>
2025-04-14 16:12:02 +0700

    Improved code

--- src/giskard_lmutils/model/litellm.py
+++ src/giskard_lmutils/model/litellm.py
@@ -14,6 +14,37 @@ try:
 except ImportError:
     TRANSFORMERS_AVAILABLE = False
 
+
+class _LocalEmbeddingModel:
+    def __init__(self, model: str):
+        if not TORCH_AVAILABLE:
+                raise ValueError("""
+                torch is not installed. Please install it with `pip install torch`.
+                This is required to use the local embedding model.
+                Alternatively, you can use the remote embedding model by setting `is_local=False` in the embedding_params.
+                """)
+        
+        if not TRANSFORMERS_AVAILABLE:
+            raise ValueError("""
+            transformers is not installed. Please install it with `pip install transformers`.
+            This is required to use the local embedding model.
+            Alternatively, you can use the remote embedding model by setting `is_local=False` in the embedding_params.
+            """)
+        
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = AutoModel.from_pretrained(self.model_name).to(self.device)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+
+    def get_embedding(self, input: str):
+        inputs = self.tokenizer(
+            input, return_tensors="pt", truncation=True, padding=True
+        ).to(self.device)
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        return outputs.last_hidden_state.mean(dim=1).squeeze(0)
+    
 class LiteLLMModel:
 
     def __init__(self, 
@@ -28,25 +59,11 @@ class LiteLLMModel:
         if completion_model is None and embedding_model is None:
             raise ValueError("Either completion_model or embedding_model must be provided")
         
-        if completion_params.get('is_local', False) and not TORCH_AVAILABLE:
-            raise ValueError("""
-            torch is not installed. Please install it with `pip install torch`.
-            This is required to use the local embedding model.
-            Alternatively, you can use the remote embedding model by setting `is_local=False` in the embedding_params.
-            """)
-        
-        if embedding_params.get('is_local', False) and not TRANSFORMERS_AVAILABLE:
-            raise ValueError("""
-            transformers is not installed. Please install it with `pip install transformers`.
-            This is required to use the local embedding model.
-            Alternatively, you can use the remote embedding model by setting `is_local=False` in the embedding_params.
-            """)
+        if embedding_params is not None and embedding_params.get('is_local', False):
+            self._local_embedding_model = _LocalEmbeddingModel(embedding_model)
 
         self._completion_params = {**(completion_params or {}), 'model': completion_model }
         self._embedding_params = {**(embedding_params or {}), 'model': embedding_model }
-        self.model = None
-        self.tokenizer = None
-        self.device = None
 
     def _build_completion_params(self, completion_params, messages):
         return {**self._completion_params, **completion_params, 'messages': messages}
@@ -63,34 +80,18 @@ class LiteLLMModel:
         completion_params = self._build_completion_params(completion_params, messages)
 
         return await acompletion(**completion_params)
-    
 
     
-    def _local_embed(self, input: list[str], **embedding_params):
-        if self.model is None:
-            self.device = "cuda" if torch.cuda.is_available() else "cpu"
-            self.tokenizer = AutoTokenizer.from_pretrained(embedding_params['model'])
-            self.model = AutoModel.from_pretrained(embedding_params['model']).to(self.device)
-
-        def _get_embedding(input: str):
-            inputs = self.tokenizer(
-                input, return_tensors="pt", truncation=True, padding=True
-            ).to(self.device)
-
-            with torch.no_grad():
-                outputs = self.model(**inputs)
-            return outputs.last_hidden_state.mean(dim=1).squeeze(0)
-
+    def _local_embed(self, input: list[str]):
         return {"data": [
-            { "embedding": torch.stack([_get_embedding(d)]).flatten().tolist()} for d in input
+            { "embedding": torch.stack([self._local_embedding_model.get_embedding(d)]).flatten().tolist()} for d in input
             ]}
 
-    
     def embed(self, input: list[str], **embedding_params):
         embedding_params = self._build_embedding_params(embedding_params, input)
 
         if embedding_params.get('is_local', False):
-            return self._local_embed(**embedding_params)
+            return self._local_embed(input)
 
         return embedding(**embedding_params)
     
@@ -98,6 +99,6 @@ class LiteLLMModel:
         embedding_params = self._build_embedding_params(embedding_params, input)
 
         if embedding_params.get('is_local', False):
-            return self._local_embed(**embedding_params)
+            return self._local_embed(input)
 
         return await aembedding(**embedding_params)
