b05228b1bbe2b1bd458b9a745eaa1acc33706bbf
Kevin Messiaen <kevin.messiaen@icloud.com>
2025-04-15 10:04:25 +0700

    Added docstring

--- src/giskard_lmutils/clustering/find_topic.py
+++ src/giskard_lmutils/clustering/find_topic.py
@@ -24,8 +24,21 @@ async def find_topic(
         topic_document_count: int = 10, # TODO: move in config?
         seed: int = 1729
 ) -> str:
-    LOGGER.debug("Create topic name from topic documents")
+    """
+    Find a topic for a set of documents.
 
+    Args:
+        model (LiteLLMModel): The model to use for completion.
+        topic_documents (list[str]): The documents to find a topic for.
+        language (str): The language of the topic.
+        document_max_length (int): The maximum length of a document. This is used to limit the size of the documents used for the topic.
+        topic_document_count (int): The number of documents to use for the topic. This is used to limit the size of the documents used for the topic.
+        seed (int): The seed for the random number generator.
+
+    Returns:
+        str: The topic.
+    """
+    LOGGER.debug("Create topic name from topic documents")
     rng = np.random.default_rng(seed=seed)
     rng.shuffle(topic_documents)
     topics_str = "\n\n".join(
